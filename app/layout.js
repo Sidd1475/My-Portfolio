dfafdea6595a092cb13c5167d192b3d3973cb21f
Sidd1475 <167200325+Sidd1475@users.noreply.github.com>
2025-07-06 02:39:38 +0530

    Add Background component and fix import path

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import Background from "@/components/background";
+import Background from "@/components/Background";
 
 
 
